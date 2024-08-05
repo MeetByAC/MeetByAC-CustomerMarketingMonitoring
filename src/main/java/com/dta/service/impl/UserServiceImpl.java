d68c81f48d1b9f13a36fea6c0dc3de70a5bea8ae
edelweiss <779443738@qq.com>
2024-08-05 16:32:33 +0800

    修改添加用户时设置默认密码的bug

--- src/main/java/com/dta/service/impl/UserServiceImpl.java
+++ src/main/java/com/dta/service/impl/UserServiceImpl.java
@@ -56,7 +56,9 @@ public class UserServiceImpl implements UserService {
      */
     @Override
     public void save(User user) {
-        user.setPassword("123456");//这不是固定了密码了吗？
+        if(user.getPassword() == null || user.getPassword() == ""){
+            user.setPassword("123456");
+        }
         user.setCreateTime(LocalDateTime.now());
         user.setUpdateTime(LocalDateTime.now());
         userMapper.insert(user);
